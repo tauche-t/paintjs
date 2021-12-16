d5ecab60a2ff9a8ecf260cd26be5086fd2119059
tauche-t <tauche.t3473@gmail.com>
2021-12-16 14:28:40 +0900

    paintingJS

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName('jsColor');
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const save = document.getElementById("jsSave");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -38,6 +46,7 @@ function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
 
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -56,11 +65,31 @@ function handleModeClick() {
   }
 }
 
+function handleCanvasClick() {
+  if(filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
+function handleRightClick(event) {
+  event.preventDefault();
+}
+
+function handleSaveClick() {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS";
+  link.click();
+}
+
 if(canvas) {
   canvas.addEventListener("mousemove", onMouseEnter);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleRightClick);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
@@ -71,4 +100,8 @@ if(range) {
 
 if(mode) {
   mode.addEventListener("click", handleModeClick);
+}
+
+if(save) {
+  save.addEventListener("click", handleSaveClick);
 }
